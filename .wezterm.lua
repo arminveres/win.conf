981743b013e6e53b1cc16ebb4c2cfab09cf7089e
Armin Veres <armin.veres@hotmail.com>
2023-02-03 11:06:58 +0100

    Updated WezTerm colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,18 @@
 local wezterm = require("wezterm")
+
+-- Custom color
+local scheme = wezterm.color.get_builtin_schemes()["Gruvbox dark, hard (base16)"]
+scheme.background = "#000000"
+
 return {
-	automatically_reload_config = true,
-	-- font = wezterm.font({ family = "JetBrainsMonoNerdFont" }),
-	font = wezterm.font({ family = "Iosevka NFM" }),
 	font_size = 11.0,
-	default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe --login" },
-	color_scheme = "Gruvbox dark, hard (base16)",
+	color_schemes = { ["MyGruvbox"] = scheme },
+	color_scheme = "MyGruvbox",
+	automatically_reload_config = true,
+	window_background_opacity = 0.90,
 	enable_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
+	-- NOTE: Windows specific configs
+	default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe --login" },
+	font = wezterm.font({ family = "Iosevka NFM" }),
 }
