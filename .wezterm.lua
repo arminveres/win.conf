ec2131773b07c64845f1a659b217ee70d23b48a0
Armin Richard Veres <arminrichard.veres@varian.com>
2024-12-19 15:55:59 +0100

    feat: update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,33 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
 -- Custom color
 local scheme = wezterm.color.get_builtin_schemes()["Gruvbox dark, hard (base16)"]
 scheme.background = "#000000"
+config.color_schemes = { ["MyGruvbox"] = scheme }
+config.color_scheme = "MyGruvbox"
 
-return {
-	font_size = 11.0,
-	color_schemes = { ["MyGruvbox"] = scheme },
-	color_scheme = "MyGruvbox",
-	automatically_reload_config = true,
-	window_background_opacity = 0.90,
-	enable_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = true,
-	-- NOTE: Windows specific configs
-	default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe --login" },
-	font = wezterm.font({ family = "Iosevka NFM" }),
+config.font = wezterm.font_with_fallback({ "Terminess Nerd Font Propo", "Iosevka Nerd Font Mono" })
+config.font_size = 12
+config.default_prog = { "pwsh" }
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+	{
+		key = "F",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.Search({ CaseInSensitiveString = "" }),
+	},
 }
+
+
+config.wsl_domains = {
+	{
+		name = "WSL:Ubuntu",
+		distribution = "Ubuntu",
+		default_prog = { "tmux" }
+	}
+}
+config.default_domain = "WSL:Ubuntu"
+
+return config
